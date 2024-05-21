d06239825820eff6532d461ea258c4ec1cca233a
Sapphire Smith <ssapphire527@gmail.com>
2024-05-21 16:32:08 +0530

    connected frontend with backend and implimented all basic functionality

--- frontend/src/components/message/Messages.jsx
+++ frontend/src/components/message/Messages.jsx
@@ -1,16 +1,32 @@
+import useGetMessages from "../../hooks/useGetMessage"
 import Message from "./Message"
+import MessageSkeleton from "../skeletons/MessageSkeleton"
+import { useEffect, useRef } from "react";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behaviour: "smooth" })
+    }, 200)
+  }, [messages])
   return (
     <div className="px-4 flex-1 overflow-auto">
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+      {
+        !loading && messages.length > 0 && messages.map((message) => (
+          <div key={message._id}
+            ref={lastMessageRef}
+          >
+            <Message message={message} />
+          </div>
+        ))
+      }
+      {loading && [...Array(3)].map((_, id) => <MessageSkeleton key={id} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
     </div>
   )
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
-
+import { useState } from "react";
+import { Link } from "react-router-dom"
+import useLogin from "../../hooks/useLogin";
 const Login = () => {
+
+    const [userName, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { loading, login } = useLogin()
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await login(userName, password)
+    }
+
     return (
         <div className="flex felx-col items-center justify-center min-w-96 mx-auto">
             <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg
@@ -10,24 +22,34 @@ const Login = () => {
                     <span className="text-blue-500">ChatApp</span>
                 </h1>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label className="label p-2">
                             <span className="text-base label-text">Username</span>
                         </label>
-                        <input type="text" placeholder="Enter username" className="w-full input input-border h-10" />
+                        <input type="text"
+                            value={userName}
+                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="Enter username" className="w-full input input-border h-10" />
                     </div>
                     <div>
                         <label className="label p-2">
                             <span className="text-base label-text">password</span>
                         </label>
-                        <input type="text" placeholder="Enter password" className="w-full input input-border h-10" />
+                        <input type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Enter password" className="w-full input input-border h-10" />
                     </div>
-                    <a href="" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+                    <Link to='/signup' className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
                         Don't have an account?
-                    </a>
+                    </Link>
                     <div>
-                        <button className="btn btn-block btn-sm mt-2">Login</button>
+                        <button className="btn btn-block btn-sm mt-2"
+                            disabled={loading}
+                        >
+                            {loading ? <span className="loading loading-spinner"></span> : "Login"}
+                        </button>
                     </div>
                 </form>
             </div>
