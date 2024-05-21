fcccec6c490e533659399548d4ff701c17739829
Sapphire Smith <ssapphire527@gmail.com>
2024-05-21 19:09:14 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,8 @@ import connectToMongoDb from "./db/connectToMongoDB.js";
 
 const PORT = process.env.PORT || 8000
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser())
@@ -19,11 +22,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.get("/", (req, res) => {
-    res.send("Hello world..")
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
-
 
 server.listen(5000, () => {
     connectToMongoDb()
