43fb2da8d20bc99d4e68e1f8058ea9cf22bc3467
Sapphire Smith <ssapphire527@gmail.com>
2024-08-12 13:24:07 +0530

    message not receving bug

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -10,6 +10,7 @@ const useListenMessages = () => {
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
             newMessage.shouldShake = true;
+            console.log(newMessage);
             const sound = new Audio(notificationSound);
             sound.play()
             setMessages([...messages, newMessage])
